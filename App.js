ef7d080efa167f68196a4b090fb51fc1da4cbdde
hesi0522 <henrique.silva@netcracker.com>
2023-06-07 16:06:53 -0300

    improving basic layout

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} placeholder='Your course goal!' />
         <Button title='Add Goal' />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -15,18 +15,28 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#CCCCCC',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
