e2faa1c839455285e39abbccafca5417f291650c
hesi0522 <henrique.silva@netcracker.com>
2023-06-12 20:24:32 -0300

    feat: first commit

---
+++ expense-tracker-app/screens/ManageExpense.js
@@ -0,0 +1,7 @@
+import { StyleSheet, View } from 'react-native';
+
+export const ManageExpense = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
---
+++ expense-tracker-app/screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import { StyleSheet, View } from 'react-native';
+
+export const RecentExpenses = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
