dc7d1e39483b77b45b703d2d5ed20b6c8dd7e931
hesi0522 <henrique.silva@netcracker.com>
2023-06-12 20:33:08 -0300

    feat: add stacks navigation

--- expense-tracker-app/App.js
+++ expense-tracker-app/App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { ManageExpense } from './screens/ManageExpense';
+import { RecentExpenses } from './screens/RecentExpenses';
+import { AllExpenses } from './screens/AllExpenses';
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <BottomTab.Navigator>
+      <BottomTab.Screen name='RecentExpenses' component={RecentExpenses} />
+      <BottomTab.Screen name='AllExpenses' component={AllExpenses} />
+    </BottomTab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='auto' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* The first screen the loads up when the app starts */}
+          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} />
+          <Stack.Screen name='ManageExpenses' component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
