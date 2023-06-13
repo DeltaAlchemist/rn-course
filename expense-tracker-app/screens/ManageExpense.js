8a36c673ec6cd2e8b15da92edb71a246a48f7719
hesi0522 <henrique.silva@netcracker.com>
2023-06-13 15:31:03 -0300

    feat: Add Custom Buttons

--- expense-tracker-app/screens/ManageExpense.js
+++ expense-tracker-app/screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { IconButton } from '../components/ui/IconButton';
 import { GlobalStyles } from '../constants/styles';
+import { Button } from '../components/ui/Button';
 
 export const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -15,8 +16,20 @@ export const ManageExpense = ({ route, navigation }) => {
 
   const deleteExpenseHandler = () => {};
 
+  const cancelHandler = () => {};
+
+  const confirmHandler = () => {};
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        <Button style={styles.button} mode='flat' onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -36,6 +49,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
